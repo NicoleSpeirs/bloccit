6274c7215e697a5667682c3bcbd9dd5f5ba3351e
Nicole Speirs <nickispeirs@gmail.com>
2019-08-28 23:49:20 -0500

    typo corection

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,9 +4,8 @@ const base = "http://localhost:3000/";
 const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
-
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
@@ -16,7 +15,7 @@ describe("routes : static", () => {
   });
 
   describe("GET /marco", () => {
-    it("should return status code 200 & 'polo' in the body response", (done) => {
+    it("should return status code 200 & 'polo' in the body response", done => {
       request.get(base + "marco", (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(res.body).toBe("polo");
@@ -26,13 +25,12 @@ describe("routes : static", () => {
   });
 
   describe("GET /about", () => {
-    it("should return status code 200 & the string 'About Us' in the body response", (done) => {
+    it("should return status code 200 & the string 'About Us' in the body response", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(res.body).toContain('About Us');
+        expect(res.body).toContain("About Us");
         done();
       });
     });
   });
-
 });
--- src/app.js
+++ src/app.js
@@ -8,4 +8,3 @@ appConfig.init(app, express);
 routeConfig.init(app);
 
 module.exports = app;
-
